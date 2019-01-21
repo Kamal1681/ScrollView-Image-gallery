49833f492ab32c1d8592f4c6f83a7d4e2981a22d
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-21 16:30:20 -0500

    Initial Commit

---
+++ ScrollView Image Gallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView Image Gallery
+//
+//  Created by Kamal Maged on 2019-01-21.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
