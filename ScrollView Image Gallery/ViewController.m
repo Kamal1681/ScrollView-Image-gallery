0f4b114fd78408abbc9777877b2e6e9d77196893
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-21 17:38:32 -0500

    part 1

--- ScrollView Image Gallery/ViewController.m
+++ ScrollView Image Gallery/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UIScrollViewDelegate>
+@property(weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property(weak, nonatomic) IBOutlet UIPageControl *pageControl;
 @end
 
 @implementation ViewController
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGRect scrollViewBounds = self.scrollView.bounds;
+    UIImageView * image1 = [[UIImageView alloc]initWithFrame:scrollViewBounds];
+    [image1 setImage:[UIImage imageNamed:@"Lighthouse-in-Field"]];
+    [self.scrollView addSubview:image1];
+    
+    UIImageView * image2 = [[UIImageView alloc]initWithFrame:CGRectOffset(scrollViewBounds, scrollViewBounds.size.width, 0)];
+    [image2 setImage:[UIImage imageNamed:@"Lighthouse-night"]];
+    [self.scrollView addSubview:image2];
+    
+    UIImageView * image3 = [[UIImageView alloc]initWithFrame:CGRectOffset(scrollViewBounds, scrollViewBounds.size.width * 2, 0)];
+    [image3 setImage:[UIImage imageNamed:@"Lighthouse"]];
+    [self.scrollView addSubview:image3];
+    
+    self.scrollView.contentSize = CGSizeMake(scrollViewBounds.size.width * 3, scrollViewBounds.size.height);
 }
 
 
